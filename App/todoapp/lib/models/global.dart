035d6b2204a954b9c2cc19134c4ba6fd901ffb3d
zKatie0111 <64928934+zKatie0111@users.noreply.github.com>
2021-01-04 22:42:01 -0500

    Create intray top bar and button #2

---
+++ App/todoapp/lib/models/global.dart
@@ -0,0 +1,11 @@
+// contains all the different text fonts
+import 'package:flutter/material.dart';
+
+Color darkGreyColor = new Color(0xFF2D2D2D);
+Color lightBlueColor = new Color(0xFFCEE2FF);
+
+TextStyle intrayTitleStyle = new TextStyle(
+    fontFamily: 'Open Sans',
+    fontWeight: FontWeight.normal,
+    color: darkGreyColor,
+    fontSize: 50);
